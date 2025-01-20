7719abd2be2287ab22b8e10e52cf33d82ee44188
ChicoBella <nirmalgajendran@gmail.com>
2025-01-19 21:02:27 -0500

    added control statement excercise

---
+++ ControlFlow/control_flow.js
@@ -0,0 +1,52 @@
+let userRole = "admin";
+let accesslevel;
+
+if(userRole === "admin") {
+    accesslevel = "Full Access granted";
+}
+else if(userRole === "manager") {
+    accesslevel = "Limited Access granted";
+}   
+else{
+    accesslevel = "No Access granted";
+}
+
+console.log("Access Level:",accesslevel); // Full Access granted
+
+let isLoggedIn = true;
+let userMessage;
+
+
+if(isLoggedIn) {
+    if(userRole === "admin") {
+        userMessage = "Welcome Admin!";
+    }
+    else{
+        userMessage = "Welcome User!";
+    }
+    
+}
+else{
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:",userMessage); // Welcome Admin! 
+
+let userType = "subscriber";
+let userCategory;
+
+switch(userType) {
+    case "admin":
+        userCategory = "Admin User";
+        break;
+    case "manager":
+        userCategory = "Manager User";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber User";
+        break;
+    default:
+        userCategory = "Unknown User";
+}
+
+console.log("User Category:",userCategory); // Subscriber User
