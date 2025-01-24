7a7440e2dc552fb7720675c408e7155e81b08742
nirmalgajendran <nirmalgajendran@gmail.com>
2025-01-24 03:19:55 -0500

    issue fix

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback()
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userexperience = document.getElementById('userExperience').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -17,21 +18,16 @@ function submitFeedback()
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-    
+    document.getElementById('userProductExperience').innerHTML = userexperience;
+
     document.getElementById('userInfo').style.display = 'block';
 }
 
 const submitButton = document.getElementById('submitBtn');
 submitButton.onclick = submitFeedback;
 
-
-
-
-
-
 document.addEventListener('keydown',function(event){
     if(event.key === 'Enter'){
         submitFeedback();
     }
-}
-);
+});
